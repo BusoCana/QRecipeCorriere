cd6261d40cefdbbedb3f6599d6d08be2c0c1967f
m-Busin <businmattia@gmail.com>
2021-07-09 11:03:43 +0200

    "Fatto funzionare mappa" commit -bisognava solo implmentare un pò di metodi

--- app/src/main/java/com/example/qrecipecorriere/UserFragment.kt
+++ app/src/main/java/com/example/qrecipecorriere/UserFragment.kt
@@ -12,6 +12,8 @@ import android.view.ViewGroup
 import android.widget.CheckBox
 import android.widget.Toast
 import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.MapView
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
@@ -19,30 +21,33 @@ import kotlinx.android.synthetic.main.fragment_ingredients.*
 import kotlinx.android.synthetic.main.fragment_user.*
 import java.io.IOException
 
-class UserFragment : Fragment() {
+class UserFragment : Fragment(), OnMapReadyCallback {
 
     private val TAG = "UserFragment"
+
+    private val MAPVIEW_BUNDLE_KEY = "MapViewBundleKey"
+    private lateinit var mMapView: MapView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        //map
-        /*
-        mapView.onCreate(savedInstanceState)
-        mapView.getMapAsync(OnMapReadyCallback { googleMap ->
-            val userLocation = getLocationFromAddress(this.requireContext(), address)
-            googleMap.addMarker(MarkerOptions().position(userLocation).title("Marker in Reciver Location"))
-            googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLocation, 13f)) })
-        */
-
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_user, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        //map init
+        var mapViewBundle = Bundle()
+        if(savedInstanceState != null)
+            mapViewBundle = savedInstanceState.getBundle(MAPVIEW_BUNDLE_KEY)!!
+
+        mMapView = view.findViewById(R.id.mapView) as MapView
+        mapView.onCreate(mapViewBundle)
+        mapView.getMapAsync(this)
 
         //get user info from arguments
         val name = arguments?.getString("user name").toString()
@@ -57,15 +62,6 @@ class UserFragment : Fragment() {
         userAddressView.text = address
         userCellularView.text = cellular
         userEmailView.text = email
-
-        //map
-        mapView.onCreate(savedInstanceState)
-        mapView.getMapAsync(OnMapReadyCallback { googleMap ->
-            Log.v(TAG, "OnMapReadyCallback - address: $address")
-            val userLocation = getLocationFromAddress(this.requireContext(), address)
-            googleMap.addMarker(MarkerOptions().position(userLocation).title("Marker in Reciver Location"))
-            googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLocation, 13f))
-        })
 
     }
 
@@ -84,6 +80,57 @@ class UserFragment : Fragment() {
         } catch (e: IOException) { e.printStackTrace() }
 
         return p1
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        var mapViewBundle = outState.getBundle(MAPVIEW_BUNDLE_KEY)
+        if(mapViewBundle == null) {
+            mapViewBundle = Bundle()
+            outState.putBundle(MAPVIEW_BUNDLE_KEY, mapViewBundle)
+        }
+
+        mMapView.onSaveInstanceState(mapViewBundle)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mMapView.onStart()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mMapView.onStop()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mMapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mMapView.onPause()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mMapView.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mMapView.onLowMemory()
+    }
+
+    override fun onMapReady(googleMap: GoogleMap?) {
+        val address = arguments?.getString("user address").toString()
+        Log.v(TAG, "OnMapReadyCallback - address: $address")
+
+        val userLocation = getLocationFromAddress(this.requireContext(), address)
+        googleMap?.addMarker(MarkerOptions().position(userLocation).title("Marker in Reciver Location"))
+        googleMap?.moveCamera(CameraUpdateFactory.newLatLngZoom(userLocation, 13f))
     }
 
 }
